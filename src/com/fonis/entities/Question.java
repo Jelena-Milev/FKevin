9ce0a5a53f6f1746fe6b193ceb4a3e3e5c0453e5
Jelena-Milev <milev816@fon.bg.ac.rs>
2018-09-08 02:49:11 +0200

    The way of saving questions into .json file improved.
    Functionality for loading questions from .json file into List or JsonArray, adding to and removing from .json file tested for cases:
    -.json file is empty;
    -.json file is not empty but the JsonObject in it does not have property named "Questions";
    -.json file is not empty and the JsonObject in it has property named "Questions" which value is empty array;
    -.json file is not empty and the JsonObject in it has property named "Questions" which value is array that is not empty (and contains or does not contain the question given - for adding and removing functionality).

--- src/com/fonis/entities/Question.java
+++ src/com/fonis/entities/Question.java
@@ -4,11 +4,12 @@ import java.util.LinkedList;
 import java.util.Objects;
 
 public class Question {
-    public enum questionDifficulty{
+    public enum questionDifficulty {
         Low, Medium, High
     }
+
     //closed questions are pick an answers questions and open questions are fill in questions
-    public enum questionType{
+    public enum questionType {
         Open, Closed
     }
 
@@ -18,7 +19,7 @@ public class Question {
     private LinkedList<String> possibleAnswers;
     private questionDifficulty difficulty;
 
-    public Question(){
+    public Question() {
     }
 
     public String getQuestionText() {
@@ -26,7 +27,7 @@ public class Question {
     }
 
     public void setQuestionText(String questionText) {
-        if(questionText==null || questionText.isEmpty())
+        if (questionText == null || questionText.isEmpty())
             throw new RuntimeException("Question text have to be entered!");
         this.questionText = questionText;
     }
@@ -36,7 +37,7 @@ public class Question {
     }
 
     public void setCorrectAnswer(String correctAnswer) {
-        if(correctAnswer==null || correctAnswer.isEmpty())
+        if (correctAnswer == null || correctAnswer.isEmpty())
             throw new RuntimeException("The correct answer have to be entered!");
         this.correctAnswer = correctAnswer;
     }
@@ -46,11 +47,10 @@ public class Question {
     }
 
     public void setPossibleAnswers(LinkedList<String> possibleAnswers) {
-        if(type==questionType.Open){
-            if(possibleAnswers!=null && possibleAnswers.size()>0)
-             throw new RuntimeException("Check again, open questions cannot have offered answers to choose one of them!");
-        }
-        else {
+        if (type == questionType.Open) {
+            if (possibleAnswers != null && possibleAnswers.size() > 0)
+                throw new RuntimeException("Check again, open questions cannot have offered answers to choose one of them!");
+        } else {
             int indexOfInvalidAnswer;
             if (possibleAnswers == null)
                 throw new RuntimeException("There is no possible answers entered!");
@@ -59,15 +59,15 @@ public class Question {
             indexOfInvalidAnswer = possibleAnswers.indexOf(null);
             if (indexOfInvalidAnswer != -1)
                 throw new RuntimeException("Possible answer at index " + indexOfInvalidAnswer + " is null!");
-            indexOfInvalidAnswer=possibleAnswers.indexOf("");
-            if(indexOfInvalidAnswer != -1)
+            indexOfInvalidAnswer = possibleAnswers.indexOf("");
+            if (indexOfInvalidAnswer != -1)
                 throw new RuntimeException("Possible answer at index " + indexOfInvalidAnswer + " is an empty answer!");
             indexOfInvalidAnswer = possibleAnswers.indexOf(correctAnswer);
             if (indexOfInvalidAnswer != -1)
                 throw new RuntimeException("Possible answer at index " + indexOfInvalidAnswer + " is the correct answer! List with possible answers cannot contain correct answer!");
-            indexOfInvalidAnswer=duplicatedAnswer(possibleAnswers);
-            if(indexOfInvalidAnswer != -1)
-                throw new RuntimeException("Possible answer at index "+indexOfInvalidAnswer+" has a duplicate amongst other answers!");
+            indexOfInvalidAnswer = duplicatedAnswer(possibleAnswers);
+            if (indexOfInvalidAnswer != -1)
+                throw new RuntimeException("Possible answer at index " + indexOfInvalidAnswer + " has a duplicate amongst other answers!");
         }
         this.possibleAnswers = possibleAnswers;
     }
@@ -77,7 +77,7 @@ public class Question {
     }
 
     public void setDifficulty(questionDifficulty difficulty) {
-        if(difficulty==null)
+        if (difficulty == null)
             throw new RuntimeException("Difficulty of the question have to be entered!");
         this.difficulty = difficulty;
     }
@@ -87,7 +87,7 @@ public class Question {
     }
 
     public void setType(questionType type) {
-        if(type==null)
+        if (type == null)
             throw new RuntimeException("Type of the question have to be entered!");
         this.type = type;
     }
@@ -120,15 +120,17 @@ public class Question {
         return Objects.hash(type, questionText, correctAnswer, possibleAnswers, difficulty);
     }
 
-    private int duplicatedAnswer(LinkedList<String> answers){
-        for (int i=0;i<answers.size()-1;i++){
-            for(int j=i+1;j<answers.size();j++){
-                if(answers.get(i).equalsIgnoreCase(answers.get(j)))
+    private int duplicatedAnswer(LinkedList<String> answers) {
+        for (int i = 0; i < answers.size() - 1; i++) {
+            for (int j = i + 1; j < answers.size(); j++) {
+                if (answers.get(i).equalsIgnoreCase(answers.get(j)))
                     return i;
             }
         }
         return -1;
     }
+
+
 
 
 }
