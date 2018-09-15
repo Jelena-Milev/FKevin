d4551fdab46408c1c14d1e62bd608fd9cf055fb4
Jelena-Milev <milev816@fon.bg.ac.rs>
2018-09-15 23:53:37 +0200

    Added method changeValueOfPropertyInJsonFile in ParsingService class. This method creates backup file of the .json file before it changes value of a property in the original .json file.

--- src/com/fonis/services/ParsingService.java
+++ src/com/fonis/services/ParsingService.java
@@ -3,17 +3,15 @@ package com.fonis.services;
 import com.fonis.entities.Participant;
 import com.fonis.entities.Question;
 import com.fonis.resources.Resources.EntityType;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ParsingService {
@@ -27,14 +25,14 @@ public class ParsingService {
     public List getEntitiesAsList(EntityType entityType, String fileName) {
         Class type = entityType.equals(EntityType.Pariticipant) ? Participant.class : Question.class;
         Type collectionType = TypeToken.getParameterized(List.class, type).getType();
-        JsonArray entitiesInJsonArray= getEntitiesAsJsonArray(entityType, fileName);
-        List entitiesInList=gson.fromJson(entitiesInJsonArray, collectionType);
+        JsonArray entitiesInJsonArray = getEntitiesAsJsonArray(entityType, fileName);
+        List entitiesInList = gson.fromJson(entitiesInJsonArray, collectionType);
         return entitiesInList;
     }
 
 
     public JsonArray getEntitiesAsJsonArray(EntityType entityType, String fileName) {
-        String propertyName = entityType.toString()+'s';
+        String propertyName = entityType.toString() + 's';
         JsonArray entitiesInJsonArray = null;
         JsonObject jsonFileContent = getFileContentAsJsonObject(fileName);
         if (jsonFileContent != null && jsonFileContent.has(propertyName)) {
@@ -56,17 +54,48 @@ public class ParsingService {
     }
 
 
+    private void changeValueOfPropertyInJsonFile(String propertyName, JsonElement newPropertyValue,
+                                                    String fileName) {
+        backupFile(fileName);
 
+        JsonObject jsonFileContent = getFileContentAsJsonObject(fileName);
 
+//      If the file is empty, we have to create an object which we are going to write into the file.
+        if (jsonFileContent == null)
+            jsonFileContent = new JsonObject();
 
+//      If an object from the file has a property named "Questions", we have to remove it in order to add it again
+//      with a changed value. If the object has not such a property, there is nothing to be removed, and we only have
+//      to add such a property.
+        if (jsonFileContent.has(propertyName))
+            jsonFileContent.remove(propertyName);
 
+        jsonFileContent.add(propertyName, newPropertyValue);
 
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(gson.toJson(jsonFileContent));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void backupFile(String fileName){
 
+        File fileForBackup = new File(fileName);
+        String[] fileNameParts=fileForBackup.getName().split("[.]");
 
+        if(fileForBackup.exists()){
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            String currentDateTime = dateTimeFormatter.format(LocalDateTime.now());
+            File backupFile = new File("backup/"+fileNameParts[0]+"-backup-" + currentDateTime + ".json");
+            try {
+                Files.copy(fileForBackup.toPath(), backupFile.toPath());
 
-
-
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 }
