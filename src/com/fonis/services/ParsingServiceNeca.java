9e1eb75fc0bf6759723c7a55b4351f3631f1de91
Nemanja Kurcubic <nkurcubic@s4hc.com>
2018-09-18 16:13:58 +0200

    Updated parsing service, both class and json file names are now read from entity type

--- src/com/fonis/services/ParsingServiceNeca.java
+++ src/com/fonis/services/ParsingServiceNeca.java
@@ -11,9 +11,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-// #TODO  Fix saving to JSON files with multiple json objects in them, merge with other parsing services for unique parsing service
+// #TODO Fix saving to JSON files with multiple json objects in them
+// #TODO Merge with other parsing services for unique parsing service
 public class ParsingServiceNeca{
-    private Gson gson;
+    private final Gson gson;
 
     public ParsingServiceNeca(){
         this.gson = new GsonBuilder().setPrettyPrinting().create();
@@ -21,10 +22,10 @@ public class ParsingServiceNeca{
 
     /**
      * Converts a list of entities into a JSON file.
-     * @param entities List of entities to be saved to the $entityName.json file
-     * @param entityType Name of the entity
-     * @param backupOldFile If set to true, the $entityName.json file will be backed up
-     *                      to the backup folder. If set to false, the entities.json file will
+     * @param entities List of entities to be saved to the entity json file
+     * @param entityType The type of entity
+     * @param backupOldFile If set to true, the entity json file will be backed up
+     *                      to the backup folder. If set to false, the entity json file will
      *                      be overwritten with the new one.
      */
     public void parseEntitiesToJson(List<?> entities, Resources.Entities entityType, boolean backupOldFile){
@@ -38,23 +39,22 @@ public class ParsingServiceNeca{
     }
 
     /**
-     * @param type Class which will be used as the list type.
-     * @param entityName Name of the entity from which the list will be generated
-     * @return List of objects, of entered type, generated from the JSON file.
+     * @param entityType The type of entity
+     * @return List of objects, of entered entity type, generated from entity json file.
      */
-    public <T> List<T> getEntitiesJsonAsList(Class<T> type, Resources.Entities entityName){
-        JsonElement entitiesElement = this.getEntitiesJsonAsElement(entityName);
+    public <T> List<T> getEntitiesJsonAsList(Resources.Entities entityType){
+        JsonElement entitiesElement = this.getEntitiesJsonAsElement(entityType);
 
         // Convert JSON element to list
-        Type listType = TypeToken.getParameterized(List.class, type).getType();
+        Type listType = TypeToken.getParameterized(List.class, entityType.getEntityClass()).getType();
         return this.gson.fromJson(entitiesElement, listType);
     }
 
     /**
-     * Adds the entity to the currently active $entityName.json file.
+     * Adds the entity to the currently active entity json file.
      * @param entity Entity to be added.
-     * @param entityType Name of the entity to be added
-     * @param backupOldFile If set to true, a backup of the current $entityName.json file will be created.
+     * @param entityType Type of entity
+     * @param backupOldFile If set to true, a backup of the current entity json file will be created.
      */
     public void addEntityToJsonFile(Object entity, Resources.Entities entityType, boolean backupOldFile){
         JsonElement entitiesElement = this.getEntitiesJsonAsElement(entityType);
@@ -67,17 +67,17 @@ public class ParsingServiceNeca{
     }
 
     /**
-     * @param entityType Name of the entity
-     * @return The $entityName.json file converted into a JsonElement.
+     * @param entityType Type of entity
+     * @return The entity json file converted into a JsonElement.
      */
     private JsonElement getEntitiesJsonAsElement(Resources.Entities entityType){
         JsonElement entitiesElement = null;
 
-        try(JsonReader jsonReader = new JsonReader(new FileReader("resources/" + entityType.getEntityJsonFileName() + ".json"))){
+        try(JsonReader jsonReader = new JsonReader(new FileReader(Resources.DATA_LOCATION + entityType.getEntityJsonFileName()))){
             JsonObject entitiesObject = this.gson.fromJson(jsonReader, JsonObject.class);
             entitiesElement = entitiesObject.get(entityType.getEntityName());
         }catch(FileNotFoundException e){
-            System.out.println("No " + entityType.getEntityJsonFileName() + " json file available.");
+            System.out.println("No " + entityType.getEntityJsonFileName() + " file available.");
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -87,9 +87,9 @@ public class ParsingServiceNeca{
     private void writeElementToEntitiesJsonFile(JsonElement entities, Resources.Entities entityType, boolean backupOldFile){
         // Backup old file, save to new file
         if(backupOldFile){
-            this.backupEntitiesJsonFile(entityType.getEntityJsonFileName());
+            this.backupEntitiesJsonFile(entityType);
         }
-        try(Writer writer = new FileWriter("resources/" + entityType.getEntityJsonFileName() + ".json")){
+        try(FileWriter writer = new FileWriter(Resources.DATA_LOCATION + entityType.getEntityJsonFileName())){
             JsonObject entitiesObject = new JsonObject();
             entitiesObject.add(entityType.getEntityName(), entities);
             this.gson.toJson(entitiesObject, writer);
@@ -101,14 +101,14 @@ public class ParsingServiceNeca{
     /**
      * Creates the backup of the currently active $entityName.json file. The backup word is added to the file name
      * as well as the current date time in the dd-MM-yyyy HH-mm-ss format, and the file is placed in the backup folder
-     * @param entityName Name of the entity whose file should be backed up
+     * @param entityType Name of the entity whose file should be backed up
      */
-    private void backupEntitiesJsonFile(String entityName){
-        File entities = new File("resources/" + entityName + ".json");
+    private void backupEntitiesJsonFile(Resources.Entities entityType){
+        File entities = new File(Resources.DATA_LOCATION + entityType.getEntityJsonFileName());
         if(entities.exists()){
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm-ss");
             String currentDateTime = dateTimeFormatter.format(LocalDateTime.now());
-            File backupFile = new File("resources/backup/"+ entityName + "-backup-" + currentDateTime + ".json");
+            File backupFile = new File(Resources.DATA_BACKUP_LOCATION + entityType.getEntityName() + "-backup-" + currentDateTime + ".json");
             entities.renameTo(backupFile);
         }
     }
