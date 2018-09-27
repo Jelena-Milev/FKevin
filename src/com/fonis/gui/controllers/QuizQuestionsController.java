6c2e34a1b7017005261b8e10127eaccdba49b94d
Jelena-Milev <milev816@fon.bg.ac.rs>
2018-09-27 16:30:50 +0200

    Updated participant's info screen.

--- src/com/fonis/gui/controllers/QuizQuestionsController.java
+++ src/com/fonis/gui/controllers/QuizQuestionsController.java
@@ -15,17 +15,21 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import sample.Model;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
-public class QuizQuestionsController implements Initializable{
+public class QuizQuestionsController implements Initializable {
 
     @FXML
     private JFXToggleButton answerOne;
@@ -54,7 +58,7 @@ public class QuizQuestionsController implements Initializable{
     private List<AbstractQuestion> questions;
     private int currentIndex = 0;
 
-    public void buttonToggled(ActionEvent event){
+    public void buttonToggled(ActionEvent event) {
         JFXToggleButton selectedButton = (JFXToggleButton) event.getTarget();
         this.questions.get(currentIndex).setGuessedAnswer(selectedButton.getText());
 
@@ -67,7 +71,7 @@ public class QuizQuestionsController implements Initializable{
 
 
     @Override
-    public void initialize(URL location, ResourceBundle resources){
+    public void initialize(URL location, ResourceBundle resources) {
         this.resetComponents();
         this.bindNextButton();
         this.questionText.setFocusTraversable(false);
@@ -76,22 +80,22 @@ public class QuizQuestionsController implements Initializable{
         this.displayQuestion();
     }
 
-    private void displayQuestion(){
+    private void displayQuestion() {
         AbstractQuestion question = this.questions.get(this.currentIndex);
         this.resetComponents();
 
         this.loadQuestionText(question);
 //        this.questionText.setText(question.getQuestionText());
-        if(question instanceof OpenQuestion){
+        if (question instanceof OpenQuestion) {
             this.loadOpenQuestion();
-        }else{
+        } else {
             this.loadClosedQuestion((ClosedQuestion) question);
         }
 
         this.answerOne.setDisableVisualFocus(true);
     }
 
-    private void loadQuestionText(AbstractQuestion question){
+    private void loadQuestionText(AbstractQuestion question) {
         String questionText = question.getQuestionText();
 
         final IntegerProperty i = new SimpleIntegerProperty(0);
@@ -111,11 +115,11 @@ public class QuizQuestionsController implements Initializable{
         timeline.play();
     }
 
-    private void loadOpenQuestion(){
+    private void loadOpenQuestion() {
         this.guessedAnswer.setVisible(true);
     }
 
-    private void loadClosedQuestion(ClosedQuestion question){
+    private void loadClosedQuestion(ClosedQuestion question) {
         List<String> answers = new ArrayList<>();
         String[] possibleAnswers = question.getPossibleAnswers();
         answers.addAll(Arrays.asList(possibleAnswers));
@@ -130,7 +134,7 @@ public class QuizQuestionsController implements Initializable{
         this.showButtons();
     }
 
-    private void resetComponents(){
+    private void resetComponents() {
         this.answerOne.setText("");
         this.answerTwo.setText("");
         this.answerThree.setText("");
@@ -147,40 +151,40 @@ public class QuizQuestionsController implements Initializable{
         this.guessedAnswer.setVisible(false);
     }
 
-    private void deselectButtons(){
+    private void deselectButtons() {
         this.answerOne.setSelected(false);
         this.answerTwo.setSelected(false);
         this.answerThree.setSelected(false);
         this.answerFour.setSelected(false);
     }
 
-    private void showButtons(){
+    private void showButtons() {
         this.answerOne.setVisible(true);
         this.answerTwo.setVisible(true);
         this.answerThree.setVisible(true);
         this.answerFour.setVisible(true);
     }
 
-    private void bindNextButton(){
+    private void bindNextButton() {
         this.nextButton.disableProperty().bind(Bindings.isEmpty(this.guessedAnswer.textProperty()));
     }
 
-    private void updateProgressBar(){
-        double progressPercent = ((double)(this.currentIndex + 1) / this.questions.size());
+    private void updateProgressBar() {
+        double progressPercent = ((double) (this.currentIndex + 1) / this.questions.size());
         this.progressBar.setProgress(progressPercent);
     }
 
-    public void onNextButtonClicked(ActionEvent event){
-        if(this.questions.get(currentIndex) instanceof OpenQuestion){
+    public void onNextButtonClicked(ActionEvent event) throws IOException {
+        if (this.questions.get(currentIndex) instanceof OpenQuestion) {
             this.questions.get(currentIndex).setGuessedAnswer(this.guessedAnswer.getText());
         }
 
         this.updateProgressBar();
 
-        if(this.currentIndex == this.questions.size() - 2){
+        if (this.currentIndex == this.questions.size() - 2) {
             this.nextButton.setText("FINISH");
         }
-        if(this.currentIndex == this.questions.size() - 1){
+        if (this.currentIndex == this.questions.size() - 1) {
             System.out.println("MADE IT TO THE END");
             // Calculate points
             int totalPoints = Model.getNumberOfPoints(this.questions);
@@ -188,20 +192,29 @@ public class QuizQuestionsController implements Initializable{
             // Save points somewhere, possibly Static int in Model?
 
             // Switch to user info screen
-        }else{
+            this.switchToUserInfoScreen(event);
+        } else {
             ++this.currentIndex;
             this.displayQuestion();
         }
     }
 
-    public boolean canAdvanceToNextQuestion(){
-        if(!this.questions.get(this.currentIndex).getGuessedAnswer().isEmpty()){
+    public boolean canAdvanceToNextQuestion() {
+        if (!this.questions.get(this.currentIndex).getGuessedAnswer().isEmpty()) {
             return true;
         }
-        if(!this.guessedAnswer.getText().isEmpty()){
+        if (!this.guessedAnswer.getText().isEmpty()) {
             return true;
         }
 
         return false;
+    }
+
+    private void switchToUserInfoScreen(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("com/fonis/gui/fxmls/userInfoScreen.fxml"));
+        Stage currentStage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+
+        currentStage.getScene().setRoot(parent);
+        currentStage.show();
     }
 }
--- src/com/fonis/gui/controllers/UserInfoScreenController.java
+++ src/com/fonis/gui/controllers/UserInfoScreenController.java
@@ -1,4 +1,73 @@
 package com.fonis.gui.controllers;
 
-public class UserInfoScreenController {
+import com.fonis.entities.Participant;
+import com.fonis.resources.Resources;
+import com.fonis.services.ParsingService;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserInfoScreenController implements Initializable {
+
+    @FXML
+    private JFXTextField name;
+    @FXML
+    private JFXTextField surname;
+    @FXML
+    private JFXTextField email;
+    @FXML
+    private JFXTextField phoneNumber;
+    @FXML
+    private
+    JFXButton endBtn;
+    private Participant participant;
+    private ParsingService parsingService=new ParsingService();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.bindEndButton();
+    }
+    private void saveParticipantInfo() {
+        this.participant = new Participant();
+        participant.setName(this.name.getText());
+        participant.setSurname(this.surname.getText());
+        participant.setEmail(this.email.getText());
+        participant.setPhoneNumber(this.phoneNumber.getText());
+    }
+
+    private void bindEndButton() {
+        this.endBtn.disableProperty().bind(Bindings.isEmpty(name.textProperty())
+                .or(Bindings.isEmpty(surname.textProperty()))
+                .or(Bindings.isEmpty(email.textProperty()))
+        );
+    }
+
+    public void onEndButtonClicked(ActionEvent event) throws IOException {
+        this.saveParticipantInfo();
+        this.parsingService.addEntityToJsonFile(this.participant, Resources.Entities.PARTICIPANT, true);
+        this.showThanksScene(event);
+
+    }
+
+    private void showThanksScene(ActionEvent event) throws IOException {
+        Parent parent= FXMLLoader.load(getClass().getClassLoader().getResource("com/fonis/gui/fxmls/thanksScene.fxml"));
+        Stage currentStage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        currentStage.getScene().setRoot(parent);
+        currentStage.show();
+    }
+
 }
